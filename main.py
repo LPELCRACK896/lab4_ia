7fa3350b41fd989ecf0e9993d271abf926fec58d
JessicaOI <70552512+JessicaOI@users.noreply.github.com>
2023-02-21 18:25:43 -0600

    Sin librerias, solo para el 1.4

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
 
 def main():
     # Task 1.1: Leer el archivo CSV y almacenarlo en un array
     headers = ["id", "date", "price", "bedrooms", "bathrooms", "sqft_living", "sqft_lot", "floors", "waterfront", "view", "condition", "grade", "sqft_above", "sqft_basement", "yr_built", "yr_renovated", "zipcode", "lat", "long", "sqft_living15", "sqft_lot15"]
     data = np.genfromtxt('./data/kc_house_data.csv', delimiter=',', skip_header=1)
-    # data = pd.read_csv('./data/kc_house_data.csv')
 
     # Extraer la columna sqft_living y price para realizar la regresión lineal
     X = data[:, 5].reshape(-1, 1)
     y = data[:, 2].reshape(-1, 1)
 
     # Task 1.2: Ajustar un modelo polinomial en base al juego de datos
-    # Crear una instancia de PolynomialFeatures con grado máximo de 10
-    poly = PolynomialFeatures(degree=10)
+    # Definir el grado máximo del polinomio
+    max_degree = 10
 
-    # Transformar la matriz X en una matriz de características polinomiales
-    X_poly = poly.fit_transform(X)
+    # Crear una matriz X_poly que contenga las características polinomiales
+    X_poly = np.ones((X.shape[0], 1))
+    for degree in range(1, max_degree+1):
+        X_poly = np.concatenate((X_poly, np.power(X, degree)), axis=1)
 
     # Crear una instancia de LinearRegression y ajustar el modelo
-    model = LinearRegression()
-    model.fit(X_poly, y)
+    theta = np.linalg.inv(X_poly.T.dot(X_poly)).dot(X_poly.T).dot(y)
 
     # Task 1.3: Implementar el algoritmo de regresión lineal con descenso del gradiente
     # Definir la tasa de aprendizaje y el número máximo de iteraciones
@@ -31,30 +30,37 @@ def main():
     max_iters = 10000
 
     # Inicializar los parámetros con valores aleatorios
-    theta = np.random.rand(X_poly.shape[1], 1)
+    theta_gd = np.random.rand(X_poly.shape[1], 1)
 
     # Implementar el descenso del gradiente
     for i in range(max_iters):
-        h = X_poly.dot(theta)
+        h = X_poly.dot(theta_gd)
         error = h - y
         gradient = X_poly.T.dot(error)
-        theta = theta - alpha * gradient
+        theta_gd = theta_gd - alpha * gradient
 
     # Task 1.4: Usar cross-validation para encontrar el grado del polinomio que mejor describe los datos
     degrees = range(1, 20)
     mse_scores = []
 
     for degree in degrees:
-        poly = PolynomialFeatures(degree=degree)
-        X_poly = poly.fit_transform(X)
-        model = LinearRegression()
-        mse = -1 * cross_val_score(model, X_poly, y, cv=5, scoring='neg_mean_squared_error').mean()
-        mse_scores.append(mse)
+        # Crear una matriz X_poly que contenga las características polinomiales
+        X_poly_cv = np.ones((X.shape[0], 1))
+        for i in range(1, degree+1):
+            X_poly_cv = np.concatenate((X_poly_cv, np.power(X, i)), axis=1)
+
+        # Calcular el MSE utilizando validación cruzada con 5 particiones
+        mse = 0
+        for j in range(5):
+            X_train, X_test, y_train, y_test = train_test_split(X_poly_cv, y, test_size=0.2)
+            theta_cv = np.linalg.inv(X_train.T.dot(X_train)).dot(X_train.T).dot(y_train)
+            h_cv = X_test.dot(theta_cv)
+            mse += np.mean((h_cv - y_test)**2)
+        mse_scores.append(mse/5)
 
     best_degree = degrees[np.argmin(mse_scores)]
-    print("El mejor grado del polinomio es:", best_degree)
-
-    # Task 1.5: Realizar un análisis de los hallazgos
+   
+      # Task 1.5: Realizar un análisis de los hallazgos
     plt.plot(degrees, mse_scores)
     plt.xlabel('Grado del polinomio')
     plt.ylabel('MSE')
